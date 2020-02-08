912bf630dee828892612edcb47f0ebc29379e368
Jewook Ryu <11579865+jeiook@users.noreply.github.com>
2020-02-07 23:10:45 -0800

    video + some responsive changes

--- js/adaptive-background.js
+++ js/adaptive-background.js
@@ -1,17 +1,17 @@
 function changebg(index) {
     let e = document.body;
     if (index === 0) {
-        e.style.backgroundImage = "url('../images/srce1_pics/IMG_4276.jpeg')";
+        e.style.backgroundImage = "url('../images/backgrounds/vex_u.jpeg')";
         e.style.backgroundSize = "cover";
     } else if (index === 1) {
-        e.style.backgroundImage = "url('../images/external_pics/Turtlebot_3_burger.jpg')";
+        e.style.backgroundImage = "url('../images/backgrounds/turtlebot.jpg')";
         e.style.backgroundSize = "initial";
     } else if (index === 2) {
-        e.style.backgroundImage = "url('../images/srce1_pics/IMG_4165.jpeg')";
+        e.style.backgroundImage = "url('../images/backgrounds/cad.jpeg')";
         e.style.backgroundSize = "cover";
         e.style.backgroundPosition = "bottom";
     } else if (index === 3) {
-        e.style.backgroundImage = "url('../images/jpl-rover.jpg')";
+        e.style.backgroundImage = "url('../images/backgrounds/yourproject.jpg')";
         e.style.backgroundSize = "cover";
         e.style.backgroundPosition = "center center";
     }
